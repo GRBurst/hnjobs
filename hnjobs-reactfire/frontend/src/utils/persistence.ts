4b39cd7a1e70aacbcdf6d8f167df661f825ce2f6
GRBurst <GRBurst@protonmail.com>
2024-05-02 21:07:08 +0200

    minor fix is comment persistence

--- hnjobs-reactfire/frontend/src/utils/persistence.ts
+++ hnjobs-reactfire/frontend/src/utils/persistence.ts
@@ -3,15 +3,18 @@ import { pipe, Effect } from "effect"
 import axios from 'axios'
 
 const writeComments = (items: Item[][], onlyFirst = true): Effect.Effect<void, Error> => {
+    if(items === undefined || items.length == 0){
+        return Effect.succeed({})
+    } 
     const itemData: Item[] = onlyFirst ? items[0] : items.flat()
     const data = itemData.map(item => item.text).join("~~~---~~~")
-    return Effect.tryPromise(() => axios.post("http://localhost:5179/comments/", data))
+    return Effect.tryPromise(() => axios.post("http://localhost:5179/comments/", {"data": data}))
 }
 
 const getComments = (): Effect.Effect<Item[], Error> => pipe(
     Effect.tryPromise(() => axios.get("http://localhost:5179/comments/")),
-    Effect.tap((response) => console.log("Mock response: ", response.data)),
     Effect.map((response) => response.data["data"].join("").split("~~~---~~~")),
+    Effect.tap((response: string[]) => console.log("Mock response: ", response)),
     Effect.map((comments: string[]) => comments.map<Item>(commentText => { return {
         text: commentText,
         by: "mock",
