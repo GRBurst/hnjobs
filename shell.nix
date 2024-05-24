2b0fe65998dc4ef5753dbaad337aa042f77d98c8
GRBurst <GRBurst@protonmail.com>
2024-05-24 15:46:51 +0200

    cleanup nix shell
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- shell.nix
+++ shell.nix
@@ -1,17 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  pname = "playground";
+  pname = "HnJobs";
   vsextensions = (with pkgs.vscode-extensions; [
-    ms-azuretools.vscode-docker
-    ms-python.python
-    ms-python.black-formatter
-    ms-python.isort
-    ms-vsliveshare.vsliveshare
-    redhat.vscode-yaml
-    streetsidesoftware.code-spell-checker
     vscodevim.vim
-    matangover.mypy
-    charliermarsh.ruff
+    ms-vsliveshare.vsliveshare
+    ms-azuretools.vscode-docker
+    streetsidesoftware.code-spell-checker
+    redhat.vscode-yaml
     dbaeumer.vscode-eslint
     esbenp.prettier-vscode
   ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
@@ -21,14 +16,8 @@ let
       version = "4.25.2";
       sha256 = "sha256-Nk/lb9ezQt6lWU1A1L6eqI4nBCEkbGyr3xK5C3BhVPw=";
     }
-    {
-          publisher = "gitpod";
-          name = "gitpod-desktop";
-          version = "0.1.2024020115";
-          sha256 = "sha256-s6le2pzRDBc/D1k06CfVTpRmrJThnpqK/YBow6kupkg=";
-    }
   ]; 
-  vscode-pallon-plugins = pkgs.vscode-with-extensions.override {
+  vscode-hnjobs-plugins = pkgs.vscode-with-extensions.override {
     vscodeExtensions = vsextensions;
   };
   python-pkgs = ps: with ps; [
@@ -48,14 +37,9 @@ pkgs.mkShell {
 
     jq yq-go
 
-    black
-    isort
-    ruff
-    mypy
-
     nodePackages.prettier
     nodePackages.eslint
 
-    vscode-pallon-plugins
+    vscode-hnjobs-plugins
   ];
 }
