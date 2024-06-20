b1a9a74bcc866b08f65baf162c7199c82a8fe2c3
GRBurst <GRBurst@protonmail.com>
2024-06-20 13:19:18 +0200

    wip

--- frontend/src/providers/HnJobs.tsx
+++ frontend/src/providers/HnJobs.tsx
@@ -1,12 +1,15 @@
 import { Schema } from "@effect/schema";
 import { ParseError } from "@effect/schema/ParseResult";
 import { Effect, Either, pipe } from "effect";
-import { DatabaseReference, ref } from "firebase/database";
+import { ref } from "firebase/database";
 import { useDatabase, useDatabaseObjectData } from "reactfire";
 import { Item, User } from "../models/Item";
 import { getItemsFromIds } from "../utils/hn";
 
-export const HnJobs = () => {
+const getHnJobs = (): Effect.Effect<
+  Either.Either<Item, ParseError>[],
+  Error
+> => {
   const whoishiring = "whoishiring";
 
   const userRef = `v0/user/${whoishiring}`;
@@ -16,27 +19,19 @@ export const HnJobs = () => {
   const endpointRef = ref(db, userRef);
 
   // Status observable
-  const { status: endpointStatus, data: userData } =
+  const { status: endpointStatus, data: user } =
     useDatabaseObjectData<User>(endpointRef);
 
   // Get thread from user submits
-  const getLastThreads = (
-    askDbRef: DatabaseReference,
-    user: User
-  ): Effect.Effect<Either.Either<Item, ParseError>[], Error> =>
-    pipe(
-      getItemsFromIds(
-        askDbRef,
-        user.submitted?.slice(0, 3) ?? [],
-        (n: number) => n
-      ),
-      Effect.tap(([whoIsHiring, freelancer, whoWantsHiring]) => {
-        console.log(whoIsHiring.title);
-        console.log(freelancer.title);
-        console.log(whoWantsHiring.title);
-      }),
-      Effect.map((asks) =>
-        asks.map((ask) => Schema.decodeUnknownEither(Item)(ask))
-      )
-    );
+  return pipe(
+    getItemsFromIds(dbRef, user.submitted?.slice(0, 3) ?? [], (n: number) => n),
+    Effect.tap(([whoIsHiring, freelancer, whoWantsHiring]) => {
+      console.log(whoIsHiring.title);
+      console.log(freelancer.title);
+      console.log(whoWantsHiring.title);
+    }),
+    Effect.map((asks) =>
+      asks.map((ask) => Schema.decodeUnknownEither(Item)(ask))
+    )
+  );
 };
